415591e98cfff9b13e661be6c53b0e3ad86c0429
Vladyslav Latsko <vladislav.lacko@gmail.com>
2022-01-13 16:13:17 +0100

    commit

---
+++ 04-board-sources/app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#3a0d6d', '#8e44ad', '#86287c', '#501155']
+const SQUARES_NUM = 749
+
+for (let i = 0; i < SQUARES_NUM; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
